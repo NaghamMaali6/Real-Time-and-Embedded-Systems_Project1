c8f3d4cf0362162397869d014a885f5231df2f6c
Nagham Maali <naghammaali77@gmail.com>
2026-04-26 21:53:54 +0300

    Delete project directory

--- project/include/controller.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-
-#include "config.h"
-#include "furniture.h"
-
-void run_controller(Config *cfg) ;  //Main controller logic (initialization, team creation, monitoring)
-
-#endif
